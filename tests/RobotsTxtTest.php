f7760197ca2958eb5ab085c760bb6810923b4349
Nick Moline <nickmoline@gmail.com>
2016-12-23 16:04:35 -0800

    tests for RobotsTxt handler

---
+++ tests/RobotsTxtTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace NickMoline\Robots\Test;
+
+use PHPUnit_Framework_TestCase;
+use NickMoline\Robots\RobotsTxt;
+
+class RobotsTxtTest extends PHPUnit_Framework_TestCase
+{
+    private $robotsFileContents;
+
+    public function setUp()
+    {
+        $this->robotsFileContents = trim("
+User-agent: ia_archiver
+Disallow: /
+
+User-agent: bingbot
+Disallow: /blocked
+Disallow: /blockbing
+Allow: /allowgoogle
+
+User-agent: *
+Disallow: /blocked
+Disallow: /allowbing
+Disallow: /*.txt
+Allow: /banana/split.txt
+Allow: /
+
+Sitemap: http://www.example.com/sitemap.xml");
+    }
+    /**
+     * Test that if no robots.txt file is present, the url is shown as allowed
+     */
+    public function testAmbiguous()
+    {
+        $robots = new RobotsTxt("http://www.example.com/ambiguous");
+        $robots->setRobotsEmpty();
+
+        $this->assertTrue($robots->validate());
+    }
+
+    /**
+     * Test that if robots.txt file hits final Allow: /
+     */
+    public function testAllowed()
+    {
+
+        $robots = new RobotsTxt("http://www.example.com/allowed");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+        $this->assertTrue($robots->validate());
+    }
+
+    /**
+     * Test that if robots.txt is set to Disallow globally
+     */
+    public function testBlocked()
+    {
+        $robots = new RobotsTxt("http://www.example.com/blocked");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+        $this->assertFalse($robots->validate());
+    }
+
+    /**
+     * Test that if robots.txt Disallows to one user agent but allows to others
+     */
+    public function testSpecificDenied()
+    {
+        $robots = new RobotsTxt("http://www.example.com/blockbing");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+
+        $this->assertTrue($robots->validate());
+        $this->assertFalse($robots->setUserAgent("bingbot")->validate());
+    }
+
+    /**
+     * Test that if robots.txt Allows to one user agent but disallows to others
+     */
+    public function testSpecificAllowed()
+    {
+        $robots = new RobotsTxt("http://www.example.com/allowbing");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+
+        $this->assertFalse($robots->validate());
+        $this->assertTrue($robots->setUserAgent("bingbot")->validate());
+    }
+
+    public function testBlockedWildcard()
+    {
+        $robots = new RobotsTxt("http://www.example.com/banana.txt");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+        $this->assertFalse($robots->validate());
+    }
+
+    public function testWildcardException()
+    {
+        $robots = new RobotsTxt("http://www.example.com/banana/split.txt");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+        $this->assertTrue($robots->validate());
+    }
+
+    public function testSubpath()
+    {
+        $robots = new RobotsTxt("http://www.example.com/blocked/2.html");
+        $robots->setRobotsHandler($this->robotsFileContents);
+
+        $this->assertFalse($robots->validate());
+    }
+
+}
